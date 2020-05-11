b785b08830631f932b3ff0d6e7a63e9217589cb5
= <=>
2020-05-11 15:13:08 +0530

    Added ajax in form

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,3 +2,19 @@
     //= require jquery_ujs
     //= require turbolinks
     //= require tree .
+
+('#new_participant').submit(function() {
+    alert('Handler for .submit() called.');
+    return false;
+});
+
+$('#new_participant').submit(function() {
+    .preventDefault()
+    $.ajax({
+        type: 'POST',
+        url: url,
+        data: data,
+        success: success,
+        dataType: dataType
+    });
+});
